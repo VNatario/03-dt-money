356fe60210beb556939185e8962a246f793842cf
Vitor Natario <natariovitor@gmail.com>
2023-09-24 14:55:45 -0300

    styles: add theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
-  return <h1>DT MOney</h1>
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <h1>DT MOney</h1>
+    </ThemeProvider>
+  )
 }
