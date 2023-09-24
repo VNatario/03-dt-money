57386e223ead6358685988abd468d48348ec5a99
Vitor Natario <natariovitor@gmail.com>
2023-09-24 14:54:09 -0300

    styles: create deafult Theme

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,28 @@
+export const defaultTheme = {
+  colors: {
+    ['green-dark']: '#015F43',
+    ['green']: '#00875F',
+    ['green-light']: '#00B37E',
+
+    ['red-dark']: '#AA2834',
+    ['red']: '#F75A68',
+
+    ['background']: '#121214',
+    ['main-shape']: '#202024',
+    ['secondary-shape']: '#29292E',
+    ['tertiary-shape']: '#323238',
+    ['placeholder']: '#7C7C8A',
+    ['base-text']: '#C4C4CC',
+    ['titles']: '#E1E1E6',
+
+    ['white']: '#FFFFFF',
+  },
+
+  fontSize: {
+    xs: '0.875rem',
+    s: '1rem',
+    m: '1.25rem',
+    lg: '1.5rem',
+    xl: '2rem',
+  },
+}
