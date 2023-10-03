cc3a6e9b32220e0faf93a874fb8bdfe4e5f66f7b
Vitor Natario <natariovitor@gmail.com>
2023-10-03 16:25:55 -0300

    reafctor: chande fetch to axios

--- src/context/TransactionsContext.tsx
+++ src/context/TransactionsContext.tsx
@@ -1,4 +1,5 @@
 import { createContext, ReactNode, useState, useEffect } from 'react'
+import { api } from '../lib/axios'
 
 interface Transaction {
   id: number
@@ -26,15 +27,11 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
 
   async function fetchTransactions(query?: string) {
-    const url = new URL('http://localhost:3000/transactions')
+    const response = await api.get('transactions', {
+      params: { q: query },
+    })
 
-    if (query) {
-      url.searchParams.append('q', query)
-    }
-    const response = await fetch(url)
-    const data = await response.json()
-
-    setTransactions(data)
+    setTransactions(response.data)
   }
   useEffect(() => {
     fetchTransactions()
